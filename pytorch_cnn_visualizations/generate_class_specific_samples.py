7a1772315e10e5d343b18db9eac2c416d990438a
Johannes Rieke <johannes.rieke@gmail.com>
2017-12-12 17:03:22 +0100

    Added initial experiments

---
+++ pytorch_cnn_visualizations/generate_class_specific_samples.py
@@ -0,0 +1,62 @@
+"""
+Created on Thu Oct 26 14:19:44 2017
+
+@author: Utku Ozbulak - github.com/utkuozbulak
+"""
+import os
+import cv2
+import numpy as np
+
+from torch.optim import SGD
+from torchvision import models
+
+from misc_functions import preprocess_image, recreate_image
+
+
+class ClassSpecificImageGeneration():
+    """
+        Produces an image that maximizes a certain class with gradient ascent
+    """
+    def __init__(self, model, target_class):
+        self.mean = [-0.485, -0.456, -0.406]
+        self.std = [1/0.229, 1/0.224, 1/0.225]
+        self.model = model
+        self.model.eval()
+        self.target_class = target_class
+        # Generate a random image
+        self.created_image = np.uint8(np.random.uniform(0, 255, (224, 224, 3)))
+        cv2.imwrite('generated/initial_image.jpg', self.created_image)
+        # Create the folder to export images if not exists
+        if not os.path.exists('generated'):
+            os.makedirs('generated')
+
+    def generate(self):
+        initial_learning_rate = 6
+        for i in range(1, 200):
+            # Process image and return variable
+            self.processed_image = preprocess_image(self.created_image)
+            # Define optimizer for the image
+            optimizer = SGD([self.processed_image], lr=initial_learning_rate)
+            # Forward
+            output = self.model(self.processed_image)
+            # Target specific class
+            class_loss = -output[0, self.target_class]
+            print('Iteration:', str(i), 'Loss', "{0:.2f}".format(class_loss.data.numpy()[0]))
+            # Zero grads
+            self.model.zero_grad()
+            # Backward
+            class_loss.backward()
+            # Update image
+            optimizer.step()
+            # Recreate image
+            self.created_image = recreate_image(self.processed_image)
+            # Save image
+            cv2.imwrite('generated/iteration_'+str(i)+'.jpg', self.created_image)
+        return self.processed_image
+
+
+if __name__ == '__main__':
+    target_class = 130  # Flamingo
+    pretrained_model = models.alexnet(pretrained=True)
+    cig = ClassSpecificImageGeneration(pretrained_model, target_class)
+    cig.generate()
