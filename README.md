a66a80721aa03b8b3cb18d58147cf4a8fe8fbbb4
Johannes Rieke <johannes.rieke@gmail.com>
2018-03-27 15:13:13 +0200

    Added complete 1.5T data, combined 1.5T and 3T data, non-lateralized results

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cnn-interpretability
 Interpreting CNN classifications on MRI images
+
+# Requirements
+
+
+# Quickstart
+
+
+# I'm not using pytorch
+If your model is not in pytorch, but you still want to use these interpretation methods, you can try to transform the model to pytorch ([overview of conversion tools](https://github.com/ysh329/deep-learning-model-convertor)).
+
+For keras to pytorch, I can recommend [nn-transfer](https://github.com/gzuidhof/nn-transfer). If you use it, keep in mind that by default, pytorch uses channels-first format and keras channels-last format for images. Even though nn-transfer takes care of this difference for the orientation of the convolution kernels, you may still need to permute your dimensions in the pytorch model between the convolutional and fully-connected stage (for 3D images, I did `x = x.permute(0, 2, 3, 4, 1).contiguous()`). The safest bet is to switch keras to use channels-first as well, then nn-transfer should handle everything by itself.
--- utils.py
+++ utils.py
@@ -41,23 +41,25 @@ def plot_learning_curve(history):
 
 
 
-# TODO: Does z_factor in combination with mask make sense (because it wasn't included in load_masked_nifti)?
-# TODO: If the masking operation is to performance intenstive, move it to the gpu via pytorch.
 def load_nifti(file_path, mask=None, z_factor=None, remove_nan=True):
     """Load a 3D array from a NIFTI file."""
     img = nib.load(file_path)
     struct_arr = np.array(img.get_data())
+    
     if remove_nan:
-        struct_arr = np.nan_to_num(struct_arr)#.nan_to_num(copy=False)
-        # TODO: If this ever gives an error again, change it to the alternative below.
-            #    struct_arr[np.where(np.isnan(struct_arr))] = 0
+        struct_arr = np.nan_to_num(struct_arr)
     if mask is not None:
-        # TODO: Maybe use numpy's masked array here? Check out if it is faster.
         struct_arr *= mask
     if z_factor is not None:
         struct_arr = np.around(zoom(struct_arr, z_factor), 0)
 
     return struct_arr
+
+
+def save_nifti(file_path, struct_arr):
+    """Save a 3D array to a NIFTI file."""
+    img = nib.Nifti1Image(struct_arr, np.eye(4))
+    nib.save(img, file_path)
 
 
 # Transparent colormap (alpha to red), that is used for plotting an overlay.
